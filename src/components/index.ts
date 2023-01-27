c6bed1b2f8a24565e5da2359f26737655441bce4
Laura de Cos <lauradecc@gmail.com>
2023-01-27 08:19:54 +0100

    modidy modaltitleandx component archive to be in lowercase

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export * from './severity-pill';
 export * from './marketing-card';
 export * from './progress-pointer';
 export * from './progress-bar';
-export * from './ModalTitleAndX';
+export * from './modal-title-and-x';
