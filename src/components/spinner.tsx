95393bde6a61a3c62129cf34fd52425360904e83
Laura de Cos <lauradecc@gmail.com>
2023-01-31 10:44:24 +0100

    feat: add theme to spinner

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,16 +1,19 @@
 import { Box, CircularProgress } from '@mui/material';
 import { FC } from 'react';
+import { Theme } from './theme';
 
 export const Spinner: FC = () => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        width: '100%',
-        justifyContent: 'center'
-      }}
-    >
-      <CircularProgress color="primary" />
-    </Box>
+    <Theme>
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'center'
+        }}
+      >
+        <CircularProgress color="primary" />
+      </Box>
+    </Theme>
   );
 };
