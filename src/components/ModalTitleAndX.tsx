7ee4f23edf7c5a958e8e9e2d19fc2d81eb47bb12
Laura de Cos <lauradecc@gmail.com>
2023-01-31 17:55:12 +0100

    feat: add margin bottom to modaltitleandx

--- src/components/ModalTitleAndX.tsx
+++ src/components/ModalTitleAndX.tsx
@@ -8,15 +8,17 @@ type TModalTitleAndXProps = {
   text: string;
   onClick: () => void;
   textVariant?: TypographyVariant;
+  mb?: number;
 };
 
-export const ModalTitleAndX: FC<TModalTitleAndXProps> = ({ text, onClick, textVariant }) => {
+export const ModalTitleAndX: FC<TModalTitleAndXProps> = ({ text, onClick, textVariant, mb }) => {
   return (
     <Box
       sx={{
         display: 'flex',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
+        marginBottom: mb ? `${mb}px` : ''
       }}
     >
       <Typography variant={textVariant || 'h6'} component="h2">
