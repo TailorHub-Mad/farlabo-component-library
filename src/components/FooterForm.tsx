40e2d367548591dc0ae7f32213c19183cc3f77c2
Laura de Cos <lauradecc@gmail.com>
2023-02-01 12:12:44 +0100

    feat: add theme to footerform

--- src/components/FooterForm.tsx
+++ src/components/FooterForm.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Stack } from '@mui/material';
 import { FC } from 'react';
+import { Theme } from './Theme';
 
 interface FooterFormProps {
   handleSubmit: (action: string) => void;
@@ -18,33 +19,35 @@ export const FooterForm: FC<FooterFormProps> = ({ handleSubmit, step, maxSteps, 
   };
 
   return (
-    <Box position="fixed" bottom="0" left="0" bgcolor={'#fff'} width={'100%'} zIndex={5} minHeight="74px">
-      <Box
-        sx={{
-          justifyContent: 'flex-end',
-          display: 'flex',
-          padding: ' 16px 24px',
-          boxShadow: ' 0px -1px 1px rgba(100, 116, 139, 0.06), 0px -1px 2px rgba(100, 116, 139, 0.1);'
-        }}
-      >
-        <Stack direction={'row'} spacing={'16px'}>
-          {step !== 0 && (
-            <Button variant="outlined" onClick={() => handleSubmit('back')}>
-              Atrás
-            </Button>
-          )}
+    <Theme>
+      <Box position="fixed" bottom="0" left="0" bgcolor={'#fff'} width={'100%'} zIndex={5} minHeight="74px">
+        <Box
+          sx={{
+            justifyContent: 'flex-end',
+            display: 'flex',
+            padding: ' 16px 24px',
+            boxShadow: ' 0px -1px 1px rgba(100, 116, 139, 0.06), 0px -1px 2px rgba(100, 116, 139, 0.1);'
+          }}
+        >
+          <Stack direction={'row'} spacing={'16px'}>
+            {step !== 0 && (
+              <Button variant="outlined" onClick={() => handleSubmit('back')}>
+                Atrás
+              </Button>
+            )}
 
-          {step < maxSteps ? (
-            <Button variant="contained" onClick={() => submitAndScrollTop('continue')} disabled={!isFinished}>
-              Continuar
-            </Button>
-          ) : (
-            <Button onClick={() => handleSubmit('submit')} variant="contained" disabled={!isFinished}>
-              {text}
-            </Button>
-          )}
-        </Stack>
+            {step < maxSteps ? (
+              <Button variant="contained" onClick={() => submitAndScrollTop('continue')} disabled={!isFinished}>
+                Continuar
+              </Button>
+            ) : (
+              <Button onClick={() => handleSubmit('submit')} variant="contained" disabled={!isFinished}>
+                {text}
+              </Button>
+            )}
+          </Stack>
+        </Box>
       </Box>
-    </Box>
+    </Theme>
   );
 };
