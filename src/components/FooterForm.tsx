171804633300e2089de64548ffa52f48d459a679
Laura de Cos <lauradecc@gmail.com>
2023-03-13 10:52:49 +0100

    feat: disable submit button while creating

--- src/components/FooterForm.tsx
+++ src/components/FooterForm.tsx
@@ -1,16 +1,30 @@
 import { Box, Button, Stack } from '@mui/material';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Theme } from './Theme';
 
 interface FooterFormProps {
-  handleSubmit: (action: string) => void;
+  handleSubmit: (action: string) => Promise<void>;
   step: number;
   maxSteps: number;
   text: string;
   isFinished: boolean;
 }
 
-export const FooterForm: FC<FooterFormProps> = ({ handleSubmit, step, maxSteps, text, isFinished }) => {
+export const FooterForm: FC<FooterFormProps> = ({
+  handleSubmit,
+  step,
+  maxSteps,
+  text,
+  isFinished
+}) => {
+  const [isCreating, setIsCreating] = useState<boolean>(false);
+
+  const handleSubmitBtn = async (reason: string) => {
+    setIsCreating(true);
+    await handleSubmit(reason);
+    setIsCreating(false);
+  };
+
   const submitAndScrollTop = (reason: string) => {
     handleSubmit(reason);
     setTimeout(function () {
@@ -20,13 +34,22 @@ export const FooterForm: FC<FooterFormProps> = ({ handleSubmit, step, maxSteps, 
 
   return (
     <Theme>
-      <Box position="fixed" bottom="0" left="0" bgcolor={'#fff'} width={'100%'} zIndex={5} minHeight="74px">
+      <Box
+        position="fixed"
+        bottom="0"
+        left="0"
+        bgcolor={'#fff'}
+        width={'100%'}
+        zIndex={5}
+        minHeight="74px"
+      >
         <Box
           sx={{
             justifyContent: 'flex-end',
             display: 'flex',
             padding: ' 16px 24px',
-            boxShadow: ' 0px -1px 1px rgba(100, 116, 139, 0.06), 0px -1px 2px rgba(100, 116, 139, 0.1);'
+            boxShadow:
+              ' 0px -1px 1px rgba(100, 116, 139, 0.06), 0px -1px 2px rgba(100, 116, 139, 0.1);'
           }}
         >
           <Stack direction={'row'} spacing={'16px'}>
@@ -37,11 +60,19 @@ export const FooterForm: FC<FooterFormProps> = ({ handleSubmit, step, maxSteps, 
             )}
 
             {step < maxSteps ? (
-              <Button variant="contained" onClick={() => submitAndScrollTop('continue')} disabled={!isFinished}>
+              <Button
+                variant="contained"
+                onClick={() => submitAndScrollTop('continue')}
+                disabled={!isFinished}
+              >
                 Continuar
               </Button>
             ) : (
-              <Button onClick={() => handleSubmit('submit')} variant="contained" disabled={!isFinished}>
+              <Button
+                onClick={() => handleSubmitBtn('submit')}
+                variant="contained"
+                disabled={!isFinished || isCreating}
+              >
                 {text}
               </Button>
             )}
